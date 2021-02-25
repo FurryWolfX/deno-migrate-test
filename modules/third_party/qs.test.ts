6bdeb186722f8c305c40dafff7262482f351a90b
wolfx <digimonx@qq.com>
2021-02-25 21:30:34 +0800

    test for my deno_qs

--- modules/third_party/qs.test.ts
+++ modules/third_party/qs.test.ts
@@ -1,5 +1,4 @@
-// @deno-types="https://cdn.skypack.dev/@types/qs/index.d.ts"
-import qs from "https://cdn.skypack.dev/qs";
+import { qs } from "https://deno.land/x/deno_qs@0.0.1/mod.ts";
 
 const obj = qs.parse("a=c");
 console.log(obj);
